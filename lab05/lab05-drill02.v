130f4494ca76d800d9bfd0ca42688a8451b23bbf
Kristian Padilla <ketpadilla05@gmail.com>
2025-09-23 18:40:12 +0800

    HDL-22 Lab05 Folder created and performed all laboratory drills

---
+++ lab05/lab05-drill02.v
@@ -0,0 +1,29 @@
+/*
+  Design an all-bit zero/one detector using dataflow modelling. Save the file as drill6_2.vl
+*/
+
+module AllBit(input [31:0]x, output zero, one);
+  assign zero=~(|x);
+  assign one=&x;
+endmodule
+
+module AllBitTest();
+  reg [31:0] inputX;
+  wire outputZ, outputO;
+  AllBit Abit(inputX, outputZ, outputO);
+
+  initial begin
+    $dumpfile("./out/lab05-drill02.vcd");
+    $dumpvars(0, AllBitTest);
+  end
+
+  initial fork
+    $monitor($time,,," %h",inputX," %h",outputO," %h",outputZ);
+    inputX=32'h0;
+    #1 inputX=32'h12345678;
+    #2 inputX=0;
+    #3 inputX=32'hFFFFFFFF;
+    #4 inputX=32;
+    #4 $finish;
+  join
+endmodule
